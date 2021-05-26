4b1e6b53e2b950604cbdc3ea70b683e17fa56336
jeff <1105041+jeff-dh@users.noreply.github.com>
2021-05-27 00:01:34 +0200

    removed bosl2 again

--- solid/libs/bosl2/__init__.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from ...scad_import import import_scad
-from ...helpers import calling_module
-
-bosl2_path = Path(__file__).parent / "BOSL2_scad"
-import_scad(bosl2_path, calling_module(1))
-
