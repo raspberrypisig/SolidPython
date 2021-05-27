0b87911c442ea346a0da85e6366f43f4fe1b908a
jeff <1105041+jeff-dh@users.noreply.github.com>
2021-05-27 14:47:46 +0200

    __init__.py cleanup

--- solid/__init__.py
+++ solid/__init__.py
@@ -1,20 +1,13 @@
-# Some __init__ magic so we can include all solidpython code with:
-#   from solid import *
-#   from solid.utils import *
+#only import the external interface!
+from .builtins import *
+
 from .scad_render import scad_render,\
                          scad_render_to_file,\
                          scad_render_animated,\
                          scad_render_animated_file
 
-from .object_base import OpenSCADObject,\
-                         IncludedOpenSCADObject
-
-from .builtins import *
-from .scad_import import import_scad, use
-
 from .extensions.convenience import *
 from .extensions.modifiers import *
 
-#why is / was this included here and not in solid.utils?
-#from .extensions.patch_euclid import run_euclid_patch
+from .scad_import import import_scad, use
 
