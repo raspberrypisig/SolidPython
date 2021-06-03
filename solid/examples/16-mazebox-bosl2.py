4736e5641bb7695ad55aa1d651d48f6845e74ee4
jeff <1105041+jeff-dh@users.noreply.github.com>
2021-06-03 20:16:01 +0200

    mazebox example with bosl2

---
+++ solid/examples/16-mazebox-bosl2.py
@@ -0,0 +1,39 @@
+# ======================================================
+# = add relative path to the solid package to sys.path =
+# ======================================================
+import sys
+from pathlib import Path
+solidPath = Path(__file__).absolute().parent.parent.parent.as_posix()
+sys.path.append(solidPath)
+#==================================================
+
+# this is a basic "a mazing box" (mazebox), just tried to reimplement it with
+# bosl2. It's based on https://www.thingiverse.com/thing:1481
+
+from math import pi, cos
+from solid import *
+from solid.extensions.bosl2.std import cylindrical_extrude, zcyl, zrot, tube
+
+def mazebox():
+    #the maze is 64x90 pixel
+    maze_surface = surface(file="legacy/mazebox/maze7.png", center=True, invert=True)
+    maze_projection = projection(cut=True) (maze_surface.up(10))
+
+    #make the radius of the cylinder 3 times the maze width
+    r = (64 * 3) / (2*pi)
+    maze_mask = []
+    segs = 10 #number of segments to get a "rounded" maze path
+
+    for i in range(segs):
+        maze_offset = maze_projection.offset(-1+cos(i/segs * pi /2))
+        maze_mask += cylindrical_extrude(_or=r-(2*i/segs), ir=r-(2*(i+1)/segs)) (maze_offset)
+
+    full_maze_mask = maze_mask + zrot(360/3)(maze_mask) + zrot(2*360/3)(maze_mask)
+
+    maze_tube = tube(_or=r-0.2, ir=r-2.2, h=88, center=True)
+    maze_tube_head = zcyl(r=r+5, h=10).down(40)
+
+    return maze_tube + maze_tube_head - full_maze_mask
+
+mazebox().save_as_scad()
+
