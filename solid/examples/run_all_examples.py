b395321a47c2f5d90f37ca04e54b9b0272b5062c
jeff <1105041+jeff-dh@users.noreply.github.com>
2021-06-03 03:12:49 +0200

    added run all examples

---
+++ solid/examples/run_all_examples.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+p = Path(__file__).parent
+
+for f in sorted(p.iterdir()):
+    if f.suffix == ".py" and f.name[0].isdigit() \
+                 and f.name[1].isdigit() and f.name[2] == '-':
+        print(f)
+        os.system(f'python3 {f.as_posix()}')
