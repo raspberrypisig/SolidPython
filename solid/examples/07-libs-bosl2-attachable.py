2e7799755391752e1acf50ed50337c9f90045869
jeff <1105041+jeff-dh@users.noreply.github.com>
2022-07-18 17:58:51 +0200

    added support for bosl2 attachables

---
+++ solid/examples/07-libs-bosl2-attachable.py
@@ -0,0 +1,22 @@
+from solid.extensions.bosl2 import *
+
+def cubic_barbell(s=100, anchor=CENTER, spin=0, orient=UP):
+    return attachable(anchor,spin,orient, size=[s*3,s,s]) (
+        union() (
+            xcopies(2*s)(cube(s, center=True)),
+            xcyl(h=2*s, d=s/4)
+        )
+    )
+
+#cc = cubic_barbell2(100) (
+#        attach(TOP)(cube(50)),
+#        attach(LEFT)(sphere(50))
+#    )
+
+cc = cubic_barbell(100)(
+        cube(50).attach(TOP),
+        sphere(50).attach(LEFT),
+        show_anchors(30)
+    )
+
+print(cc)
--- solid/extensions/bosl2.py
+++ solid/extensions/bosl2.py
@@ -36,3 +36,21 @@ from ..core.object_base import ObjectBase
 ObjectBase.tag = lambda self, *args, **kwargs: tag(*args, **kwargs)(self)
 ObjectBase.attach = lambda self, *args, **kwargs: attach(*args, **kwargs)(self)
 
+
+#============ attachable add =============
+#enhance the add function of the attachable OpenSCADObject so it can be used
+#properly: cf. 07-libs-bosl2-attachable.py
+attachable_default_add = attachable.add
+
+def attachable_add(self, c):
+    if len(self.children) == 0:
+        attachable_default_add(self, c)
+    elif len(self.children) == 1:
+        attachable_default_add(self, union()(c))
+    else:
+        assert(len(self.children) == 2)
+        self.children[1].add(c)
+
+attachable.add = attachable_add
+#============ attachable add end =============
+
