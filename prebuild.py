857ad79e66c5f39df62c9b62c3e3e24fc3df5b1c
ssuchter <sean-github@suchter.com>
2023-04-19 13:50:39 -0700

    Move the prebuild.py check for BOSL2 checkout because submodule moved

--- prebuild.py
+++ prebuild.py
@@ -10,7 +10,7 @@ import sys
 
 
 def check_bosl2_submodule():
-    canary_file = "./solid2/libs/BOSL2/std.scad"
+    canary_file = "./solid2/extensions/bosl2/BOSL2/std.scad"
     if not os.path.exists(canary_file):
         print(f"Cannot find {canary_file} - probably you have not initalized the BOSL submodule")
         print("You probably need to run 'git submodule init ; git submodule update'")
