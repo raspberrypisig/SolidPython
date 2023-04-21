12c589a4937912f56f54cbde45f4ac6c1684f5dd
jeff <1105041+jeff-dh@users.noreply.github.com>
2023-04-22 00:32:13 +0200

    added py_scadparser as submodule

--- prebuild.py
+++ prebuild.py
@@ -9,18 +9,20 @@ import os
 import sys
 
 
-def check_bosl2_submodule():
-    canary_file = "./solid2/extensions/bosl2/BOSL2/std.scad"
+bosl2_file = "./solid2/extensions/bosl2/BOSL2/std.scad"
+py_scadparser_file = "./solid2/libs/py_scadparser/README.md"
+
+def check_submodule(canary_file):
     if not os.path.exists(canary_file):
-        print(f"Cannot find {canary_file} - probably you have not initalized the BOSL submodule")
+        print(f"Cannot find {canary_file} - probably you have not initalized the submodules")
         print("You probably need to run 'git submodule init ; git submodule update'")
         print("     (or clone with --recurse-submodules)")
         sys.exit(1)
 
 
 def main():
-    check_bosl2_submodule()
-
+    check_submodule(bosl2_file)
+    check_submodule(py_scadparser_file)
 
 if __name__ == "__main__":
     main()
